0a45a24349c27a5cc69e2490bb78dc9a8299d72f
Subrata Sarkar <subrotosarkar32@gmail.com>
2024-01-25 01:19:57 +0530

    view sorted images and videos of your favourite manga , videos, or even photo gallery

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, abort, redirect
-import os.path
+from flask import Flask, render_template, abort, redirect, url_for
+import os
+import cv2
 app = Flask(__name__,
     static_url_path='',
     static_folder='.')
@@ -7,14 +8,23 @@ app = Flask(__name__,
 @app.route('/')
 def home():
     chapters = []
-    for file in os.listdir('.'):
+    videos = []
+    for file in sorted(os.listdir('.')):
         directory = './' + file
         if os.path.isdir(directory):
-            for filename in os.listdir(directory):
-                if filename.endswith(".png") or filename.endswith(".jpg"):
-                    chapters.append((file, directory.replace('#', '%23') + '/' + filename, file.replace('#', '%23')))
+            for filename in sorted(os.listdir(directory)):
+                if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".gif"):
+                    chapters.append((file, os.path.join(directory.replace('#', '%23'), filename), file.replace('#', '%23')))
                     break
-    return render_template('home.html', chps = chapters)
+                elif filename.endswith(".mp4"):
+                    if not os.path.isfile(os.path.join(directory, "zimage.jpg")):
+                        cap = cv2.VideoCapture(os.path.join(directory, filename))
+                        cap.set(cv2.CAP_PROP_POS_FRAMES, 5)
+                        ret, frame = cap.read()
+                        cv2.imwrite(os.path.join(directory, "zimage.jpg"), frame)
+                    videos.append((file, os.path.join(directory.replace('#', '%23'), "zimage.jpg"), file.replace('#', '%23')))
+                    break
+    return render_template('home.html', chps=chapters, vids=videos)
 
 @app.route('/search/')
 def searchNone():
@@ -23,23 +33,36 @@ def searchNone():
 @app.route('/search/<query>')
 def search(query):
     chapters = []
+    videos = []
     for file in os.listdir('.'):
         directory = './' + file
         if os.path.isdir(directory) and query in file:
-            for filename in os.listdir(directory):
-                if filename.endswith(".png") or filename.endswith(".jpg"):
-                    chapters.append((file, '.' + directory.replace('#', '%23') + '/' + filename, file.replace('#', '%23')))
+            for filename in sorted(os.listdir(directory)):
+                if filename.endswith(".png") or filename.endswith(".jpg")  or filename.endswith(".jpeg") or filename.endswith(".gif"):
+                    chapters.append((file, os.path.join(directory.replace('#', '%23'), filename), file.replace('#', '%23')))
                     break
-    return render_template('search.html', chps = chapters, qry = query)
+                elif filename.endswith(".mp4"):
+                    videos.append((file, os.path.join(directory.replace('#', '%23'), "image.jpg.kv"), file.replace('#', '%23')))
+                    break
+    return render_template('search.html', chps=chapters, vids=videos, qry=query)
 
 @app.route('/<chapter_name>')
 def read(chapter_name):
     images = []
+    videos = []
     for filename in os.listdir('./' + chapter_name):
-        if filename.endswith(".png") or filename.endswith(".jpg"): 
-            images.append('./' + chapter_name.replace('#', '%23') + '/' + filename)
+        if filename.endswith(".png") or filename.endswith(".jpg")  or filename.endswith(".jpeg") or filename.endswith(".gif"):
+            images.append(os.path.join(chapter_name.replace('#', '%23'), filename))
+        elif filename.endswith(".mp4"):
+            videos.append(os.path.join(chapter_name.replace('#', '%23'), filename))
     if len(images) > 0:
-        return render_template('read.html', imgs = images)
+        try:
+           images = sorted(images, key= lambda x : int(x.split("\\")[-1].split(".")[0]))
+        except Exception as e:
+           images = sorted(images)
+    if len(videos) > 0:
+           videos = sorted(videos)
+    return render_template('read.html', txt=chapter_name, imgs=images, vids=videos)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
